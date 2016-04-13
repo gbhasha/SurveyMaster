447598478c41ecc0185c361d28b4eefb532af0e6
Galeel <gbhasha@gmail.com>
2016-04-14 00:43:57 +0530

    Survey List Component

---
+++ src/components/SurveyListComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//SurveyList.scss');
+
+class SurveyListComponent extends React.Component {
+  render() {
+    return (
+      <div className="surveylist-component">
+        Please edit src/components///SurveyListComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+SurveyListComponent.displayName = 'SurveyListComponent';
+
+// Uncomment properties you need
+// SurveyListComponent.propTypes = {};
+// SurveyListComponent.defaultProps = {};
+
+export default SurveyListComponent;
