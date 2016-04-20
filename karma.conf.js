d0af8e1085f22ba0efe826ab97547766dd2f9024
Galeel <gbhasha@gmail.com>
2016-04-20 16:29:01 +0530

    [Build] Adding Karma Fixture to load JSON mock data | Galeel

--- karma.conf.js
+++ karma.conf.js
@@ -5,18 +5,26 @@ module.exports = function(config) {
     basePath: '',
     browsers: [ 'PhantomJS' ],
     files: [
-      'test/loadtests.js'
+      'test/loadtests.js',
+      {
+        pattern: 'test/fixtures/**/*'
+      }
     ],
     port: 8080,
     captureTimeout: 60000,
-    frameworks: [ 'mocha', 'chai' ],
+    frameworks: [ 'mocha', 'chai', 'fixture' ],
     client: {
       mocha: {}
     },
-    singleRun: true,
+    singleRun: false,
     reporters: [ 'mocha', 'coverage' ],
     preprocessors: {
-      'test/loadtests.js': [ 'webpack', 'sourcemap' ]
+      'test/loadtests.js' : [ 'webpack', 'sourcemap' ],
+      '**/*.html'         : [ 'html2js' ],
+      '**/*.json'         : [ 'json_fixtures' ]
+    },
+    jsonFixturesPreprocessor: {
+      variableName: '__json__'
     },
     webpack: webpackCfg,
     webpackServer: {
